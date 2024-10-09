022f8af1fdfea06b4bbc0f4b9fb0e74bf6d1e7f2
YuriViligiardi <yuriviligiardi01@gmail.com>
2024-10-09 09:18:18 +0200

    Comunicazione stabile con il client, attraverso input e output

--- src/main/java/it/viligiardi/Main.java
+++ src/main/java/it/viligiardi/Main.java
@@ -1,16 +1,43 @@
 package it.viligiardi;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         
-        ServerSocket ss = new ServerSocket(5673); //Il server crea un serverSocket
+        try {
 
-        Socket mySocket = ss.accept(); //Funzione per far rimanere in attesa il socket, in ascolto
+            ServerSocket ss = new ServerSocket(5637); //Il server crea un serverSocket
 
-        System.out.println("Qualcuno si è collegato");
+            Socket mySocket = ss.accept(); //Funzione per far rimanere in attesa il socket, in ascolto
+
+            System.out.println("Qualcuno si è collegato");
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(mySocket.getInputStream()));
+            DataOutputStream out = new DataOutputStream(mySocket.getOutputStream());
+
+            String stringaRicevuta = in.readLine();
+            System.out.println("La stringa ricevuta: " + stringaRicevuta);
+
+            String stringaMaiuscola = stringaRicevuta.toUpperCase();
+            out.writeBytes(stringaMaiuscola + "\n");
+            System.out.println("La stringa inviata: " + stringaMaiuscola);
+
+            mySocket.close();
+            ss.close();
+
+        } catch (Exception e) {
+
+            System.out.println(e.getMessage());
+            System.out.println("Errore durante l'instanza del server");
+            System.exit(1);
+
+        }
+        
     }
 }
