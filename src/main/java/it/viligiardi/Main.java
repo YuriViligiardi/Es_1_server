6c88528d161b22038ad88da6857a90f1455e76d0
YuriViligiardi <yuriviligiardi01@gmail.com>
2024-10-09 08:43:01 +0200

    Creazione ServerSocket e mySocket

--- src/main/java/it/viligiardi/Main.java
+++ src/main/java/it/viligiardi/Main.java
@@ -1,7 +1,16 @@
 package it.viligiardi;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        
+        ServerSocket ss = new ServerSocket(5673); //Il server crea un serverSocket
+
+        Socket mySocket = ss.accept(); //Funzione per far rimanere in attesa il socket, in ascolto
+
+        System.out.println("Qualcuno si è collegato");
     }
 }
